28f2bb738a0d67288d9a67f85147f5f1e856bfbe
Xavier Millieret <XavierMillieret@eaton.com>
2021-07-30 16:49:55 +0200

    Split test files depending on addons compilation

--- lib/test/src/MsgBusMqtt.cpp
+++ lib/test/src/MsgBusMqtt.cpp
@@ -1,5 +1,5 @@
 /*  =========================================================================
-    MsgBus.cpp - description
+    MsgBusMqtt.cpp - description
 
     Copyright (C) 2014 - 2021 Eaton
 
